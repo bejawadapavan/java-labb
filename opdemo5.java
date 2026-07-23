64ac5871b640956eef7695992ded22244d799036
pavan <bejawadapavankumargoud@gmail.com>
2026-07-23 14:55:05 +0530

    23/07

--- opdemo5.java
+++
@@ -1,9 +0,0 @@
-class opdemo5
-{
-public static void main(String args[])
-{
-System.out.println((3<4)&&(4>6));
-System.out.println((3<=3)||(4>=6));
-System.out.println(!(4==6));
-}
-}
--- opdemo6.java
+++
@@ -1,9 +0,0 @@
-class opdemo6
-{
-public static void main(String args[])
-{
-System.out.println(3&4);
-System.out.println(3|4);
-System.out.println(3^4);
-}
-}
